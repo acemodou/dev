7ea7db95c298e719fb5e83bd1e1d540f2b143000
Modou Jaw <jawxx001@umn.edu>
2018-09-11 01:38:36 -0700

    Program to cyclically rotate an array clockwise

--- node/1.js
+++ node/1.js
@@ -1,20 +1,17 @@
-// #!/usr/bin/env node -- we can use that to make executable 
-
 function printHelp() {
-    console.log("1.js (c) Modou Jaw");
+
+    console.log("2.js (c) Modou Jaw");
     console.log("");
-    console.log("usage:");
-    console.log("--help             print this help");
-    console.log("--name             say hello to {Name}");
-    console.log("");
+    console.log("usage");
+    console.log("--help                 print this help");
+    console.log("--file name = {NAME}                 say hello to {NAME}");
 }
 
 
-var name = process.argv[2];
 
-if (!name) {
-    printHelp();
-    process.exit(1);
-}
 
-console.log("Hello " + args);
+var hello = require("./helloworld.js");
+
+
+
+console.log(hello.say());
